0cedb1a3d1273babc4917e4c18d44520cff2efde
tigranmk <tgnmkrtchyan@gmail.com>
2023-11-05 15:32:19 +0400

    finish task-7

--- src/components/pages/admin/PageProductImport/PageProductImport.tsx
+++ src/components/pages/admin/PageProductImport/PageProductImport.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import API_PATHS from "~/constants/apiPaths";
 import ProductsTable from "~/components/pages/admin/PageProductImport/components/ProductsTable";
 import CSVFileImport from "~/components/pages/admin/PageProductImport/components/CSVFileImport";
@@ -6,6 +7,11 @@ import Box from "@mui/material/Box";
 import { Link } from "react-router-dom";
 
 export default function PageProductImport() {
+  
+  React.useEffect(() => {
+    localStorage.setItem('authorization_token',  `${import.meta.env.VITE_TOKEN}`)
+
+  },[])
   return (
     <Box py={3}>
       <Box mb={2} display="flex" justifyContent="space-between">
--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,8 @@ import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { theme } from "~/theme";
+import axios from "axios";
+import API_PATHS from "./constants/apiPaths";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -18,6 +20,24 @@ if (import.meta.env.DEV) {
   const { worker } = await import("./mocks/browser");
   worker.start({ onUnhandledRequest: "bypass" });
 }
+
+axios.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error && error.config.url.indexOf(API_PATHS.import) !== -1) {
+      if(localStorage.getItem('authorization_token')) {
+        alert('Authorization token is invalid');
+      } else {
+        alert('Authorization token is missing');
+      }
+      const { message } = error;
+      alert(`Import error occured: ${message}`);
+    }
+    return Promise.reject(error);
+  }
+);
 
 const container = document.getElementById("app");
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
